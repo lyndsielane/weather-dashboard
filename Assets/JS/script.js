c36b9755c446e04ec2c595eaa1c9d26772eb4df2
Lyndsie Lane <lyndsielane@yahoo.com>
2021-06-27 13:38:01 -0400

    all data in place

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -14,13 +14,16 @@ $("#search-form").on("submit", async function (e) {
     console.log(weatherData);
 
     setCurrentDay(weatherData.current, cityData.name,);
+    buildForecastCards(weatherData.daily);
+
+    $("#weather-column").removeClass("hidden");
 });
 
 function setCurrentDay(currentDay, city) {
 
     $("#city").empty().append(city);
     $("#date").empty().append(moment.unix(currentDay.dt).format("M/DD/YYYY"));
-    //$("#icon").empty().append(currentDay.weather[0].icon);
+    $("#icon").empty().append(`<img src="http://openweathermap.org/img/wn/${currentDay.weather[0].icon}@2x.png">`);
     $("#temp").empty().append(currentDay.temp);
     $("#wind").empty().append(currentDay.wind_speed);
     $("#humidity").empty().append(currentDay.humidity);
@@ -28,10 +31,26 @@ function setCurrentDay(currentDay, city) {
         
 };
 
-function buildForecastCards() {
-    
-}
+function buildForecastCards(weatherData, ) {
+    var cards = $("#cards");
+    cards.empty();
 
-function setForecastCards() {
-    var cardHolders = $("#forecast-cards");
+    for (var i = 1; i < weatherData.length - 2; i++){
+        foreDate = moment.unix(weatherData[i].dt).format("M/DD/YYYY");
+        foreIcon = weatherData[i].weather[0].icon;
+        foreTemp = weatherData[i].temp.max;
+        foreWind = weatherData[i].wind_speed;
+        foreHumidity = weatherData[i].humidity;
+
+
+        cards.append(`
+            <div class="forecast-card">
+                <p class="foredate">${foreDate}</p>
+                <p class="icon"><img src="http://openweathermap.org/img/wn/${foreIcon}@2x.png"></p>
+                <p>Temp: ${foreTemp}</p>
+                <p>Wind: ${foreWind}</p>
+                <p>Humidity: ${foreHumidity}%</p>
+            </div>
+        `);
+    }
 }
