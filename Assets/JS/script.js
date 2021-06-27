f5dfbf52c998847266fcc28073ef47838803f454
Lyndsie Lane <lyndsielane@yahoo.com>
2021-06-27 16:30:24 -0400

    finalizing app

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,26 +1,26 @@
 var apiKey = "2b99b21d7b96e0eb1368248ef3f011fd";
 var localStorageName = "cityData";
 
-//uses the search button click as the action to kick off the app functionality - waits for the remaining functions to run and prevents a default page reload.
+//calling the search button to start the app functionality. async and await used to ensure each function loads before the next to ensure full application functionality as some of the data is dependent upon others.
 $("#search-form").on("submit", async function (e) {
     e.preventDefault();
-    //pulls the city entered in the search bar and runs the city weather API for that city. Also adds the city to local Storage.
+    //pulls the city entered in the search bar and runs the city weather API for that city.
     var city = $("#city-input").val();
     
     loadCityWeather(city);
 });
-
+//functionality for the buttons in the search history to reload the current weather when clicked.
 $(document).on("click", ".city-history-button", function(event) {
     loadCityWeather(event.target.value);
 });
-
+//this API allows us to learn the latitude and longitude of the searched city
 async function loadCityWeather(city) {
     var cityData = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
         .then(response => response.json());
 
     saveCity(cityData.name);
 
-    //The latitude and longitude from the previous API is  pushed into the one call weather API, that data is then pulled into a string to use.
+    //The latitude and longitude from the previous API is pushed into the one call weather API, that data is then pulled into a string to use for the forecasts.
     var weatherData = await fetch(`http://api.openweathermap.org/data/2.5/onecall?&appid=${apiKey}&lon=${cityData.coord.lon}&lat=${cityData.coord.lat}&units=imperial&exclude=minutely,alerts,hourly`)
         .then(response => response.json());
 
@@ -30,11 +30,11 @@ async function loadCityWeather(city) {
     //Once the data runs, the columns for the visibility for the weather data column is unhidden.
     $("#weather-column").removeClass("hidden");
 }
-
+//setting functionality for the searched city history.
 function addCityHistory(city) {
-    $("#city-history").append(`<button type="button" class="btn btn-info mt-3 city-history-button" value="${city}">${city}</button>`);
+    $("#city-history").append(`<button type="button" class="btn btn-secondary mt-3 city-history-button" value="${city}">${city}</button>`);
 }
-
+//using the local storage to save the cities as a string and ensuring each city is only saved once, even if it was searched multiple times
 function saveCity(city) {
     var savedCities = JSON.parse(localStorage.getItem(localStorageName) || "{}");
 
@@ -49,7 +49,7 @@ function saveCity(city) {
 
     localStorage.setItem(localStorageName, JSON.stringify(savedCities));
 }
-
+//pulling the city history from local storage, still ensuring nothing is duplicated
 function loadSavedCities() {
     var savedCities = JSON.parse(localStorage.getItem(localStorageName) || "{}");
 
@@ -71,6 +71,20 @@ function setCurrentDay(currentDay, city) {
     $("#wind").empty().append(currentDay.wind_speed);
     $("#humidity").empty().append(currentDay.humidity);
     $("#uvi").empty().append(currentDay.uvi);
+    //adding in the uv indicator functionality so the background of the number reflects the uv scale
+    var uviColor = "green";
+
+    if (currentDay.uvi >= 3 && currentDay.uvi < 6) {
+        uviColor = "yellow";
+    } else if (currentDay.uvi >= 6 && currentDay.uvi < 8) {
+        uviColor = "orange";
+    } else if (currentDay.uvi >= 8 && currentDay.uvi < 11) {
+        uviColor = "red";
+    } else if (currentDay.uvi >= 11) {
+        uviColor = "purple";
+    }
+
+    $("#uvi").removeClass().addClass(uviColor);
 };
 //Cycling through the API array and pulling the 5-day forecast data and building the forecast cards.
 function buildForecastCards(weatherData, ) {
--- README.md
+++ README.md
@@ -6,4 +6,8 @@
 * Beneath the current conditions display is a 5-day forecast displayed in separate cards to show the predicted condition, temperature, wind, and humidity.
 * All past searched cities display beneath the search bar for convenience to pull the weather again at a later time. 
 * The weather forecasts all have a corresponding icon representing the weather condition. 
-* I used two separate API's from Open WEather to enable all of the features. The one call API gave me the data I needed, but required latitude and longitude search parameters. I was able to use a city based API for the search bar that gave the corresponding latitude and longitude, which I then pass into the one call API to pull the weather data. This allows the user to search by a city name. I also had to add in some features to my JS functions to ensure the city information was pulled and in place before my second API ran. I was able to use async and await to ensure the information pulls and passes through as needed and the site functions as required and quickly. 
+* I used two separate API's from Open Weather to enable all of the features. The one call API gave me the data I needed, but required latitude and longitude search parameters. I was able to use a city based API for the search bar that gave the corresponding latitude and longitude, which I then pass into the one call API to pull the weather data. This allows the user to search by a city name. I also had to add in some features to my JS functions to ensure the city information was pulled and in place before my second API ran. I was able to use async and await to ensure the information pulls and passes through as needed and the site functions as required and quickly. 
+
+The application is deployed at https://lyndsielane.github.io/weather-dashboard/.
+
+![app preview]()
