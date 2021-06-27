a79e96ecb8a6ebd91ec6b4421c33e349502ffd66
Lyndsie Lane <lyndsielane@yahoo.com>
2021-06-27 15:41:21 -0400

    functionality in place

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,24 +1,68 @@
 var apiKey = "2b99b21d7b96e0eb1368248ef3f011fd";
+var localStorageName = "cityData";
 
+//uses the search button click as the action to kick off the app functionality - waits for the remaining functions to run and prevents a default page reload.
 $("#search-form").on("submit", async function (e) {
     e.preventDefault();
-
+    //pulls the city entered in the search bar and runs the city weather API for that city. Also adds the city to local Storage.
     var city = $("#city-input").val();
     
-    var cityData = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
-        .then(response => response.json())
+    loadCityWeather(city);
+});
 
+$(document).on("click", ".city-history-button", function(event) {
+    loadCityWeather(event.target.value);
+});
+
+async function loadCityWeather(city) {
+    var cityData = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
+        .then(response => response.json());
+
+    saveCity(cityData.name);
+
+    //The latitude and longitude from the previous API is  pushed into the one call weather API, that data is then pulled into a string to use.
     var weatherData = await fetch(`http://api.openweathermap.org/data/2.5/onecall?&appid=${apiKey}&lon=${cityData.coord.lon}&lat=${cityData.coord.lat}&units=imperial&exclude=minutely,alerts,hourly`)
         .then(response => response.json());
 
-    console.log(weatherData);
-
+    //Two functions are set from here - currentDay which pulls the current day information and ForecastCards which is the data used for the 5-day forecast.
     setCurrentDay(weatherData.current, cityData.name,);
     buildForecastCards(weatherData.daily);
-
+    //Once the data runs, the columns for the visibility for the weather data column is unhidden.
     $("#weather-column").removeClass("hidden");
-});
+}
 
+function addCityHistory(city) {
+    $("#city-history").append(`<button type="button" class="btn btn-info mt-3 city-history-button" value="${city}">${city}</button>`);
+}
+
+function saveCity(city) {
+    var savedCities = JSON.parse(localStorage.getItem(localStorageName) || "{}");
+
+    if (!savedCities.cities) {
+        savedCities["cities"] = [];
+    }
+
+    if (!savedCities.cities.includes(city)) {
+        savedCities.cities.push(city);
+        addCityHistory(city);
+    }
+
+    localStorage.setItem(localStorageName, JSON.stringify(savedCities));
+}
+
+function loadSavedCities() {
+    var savedCities = JSON.parse(localStorage.getItem(localStorageName) || "{}");
+
+    if (!savedCities.cities) {
+        return;
+    }
+
+    savedCities.cities.forEach(function(city) {
+        addCityHistory(city);
+    }); 
+}
+
+//Pulling the current forecast data and appending it to box in place to hold it.
 function setCurrentDay(currentDay, city) {
     $("#city").empty().append(city);
     $("#date").empty().append(moment.unix(currentDay.dt).format("M/DD/YYYY"));
@@ -28,7 +72,7 @@ function setCurrentDay(currentDay, city) {
     $("#humidity").empty().append(currentDay.humidity);
     $("#uvi").empty().append(currentDay.uvi);
 };
-
+//Cycling through the API array and pulling the 5-day forecast data and building the forecast cards.
 function buildForecastCards(weatherData, ) {
     var cards = $("#cards");
     cards.empty();
@@ -37,7 +81,7 @@ function buildForecastCards(weatherData, ) {
         cards.append(`
             <div class="forecast-card">
                 <p class="foredate">${moment.unix(weatherData[i].dt).format("M/DD/YYYY")}</p>
-                <p class="icon"><img src="http://openweathermap.org/img/wn/${weatherData[i].weather[0].icon}@2x.png"></p>
+                <p class="foreicon"><img src="http://openweathermap.org/img/wn/${weatherData[i].weather[0].icon}@2x.png"></p>
                 <p>Temp: ${weatherData[i].temp.max} &deg;F</p>
                 <p>Wind: ${weatherData[i].wind_speed} MPH</p>
                 <p>Humidity: ${weatherData[i].humidity} %</p>
@@ -46,3 +90,4 @@ function buildForecastCards(weatherData, ) {
     }
 }
 
+loadSavedCities();
